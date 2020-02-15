861f86eb922f9d469118e77dca57ae78f2ff33a5
Muhammad Al Ziqri <alziqziq@gmail.com>
2020-02-16 06:24:00 +0700

    update

---
+++ demo-pokemon/precache-manifest.373bfbb0c5794e5f18e3ff57b0768c82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c7efa28f84d2df6db80127c4da9129",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f994f8a412e00357944c",
+    "url": "./static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "082ba94a5c6373188baf",
+    "url": "./static/css/main.af74f5b2.chunk.css"
+  },
+  {
+    "revision": "f994f8a412e00357944c",
+    "url": "./static/js/2.cce47105.chunk.js"
+  },
+  {
+    "revision": "5f0e88c0091fb38b081753082759f784",
+    "url": "./static/js/2.cce47105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "082ba94a5c6373188baf",
+    "url": "./static/js/main.4f5edd5d.chunk.js"
+  },
+  {
+    "revision": "0ff944970872d1606b13",
+    "url": "./static/js/runtime-main.d4575370.js"
+  },
+  {
+    "revision": "64b7ec9d075e9f838d795ab19a3d05f0",
+    "url": "./static/media/pokeball.64b7ec9d.png"
+  }
+]);
--- demo-pokemon/service-worker.js
+++ demo-pokemon/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.50a10747522da97fd571332e24a6bb2d.js"
+  "./precache-manifest.373bfbb0c5794e5f18e3ff57b0768c82.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
