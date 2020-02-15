72cca5ae53f710c83fdf67ab77e81b0f28902b0d
Muhammad Al Ziqri <alziqziq@gmail.com>
2020-02-16 06:14:21 +0700

    feat: add demo pokemon

---
+++ demo-pokemon/precache-manifest.50a10747522da97fd571332e24a6bb2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d709a03aa1933c4d0f7af06781cbfd94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71f652bddd85200dd3d0",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "920794900d9aa9865c40",
+    "url": "/static/css/main.af74f5b2.chunk.css"
+  },
+  {
+    "revision": "71f652bddd85200dd3d0",
+    "url": "/static/js/2.b909261d.chunk.js"
+  },
+  {
+    "revision": "5f0e88c0091fb38b081753082759f784",
+    "url": "/static/js/2.b909261d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "920794900d9aa9865c40",
+    "url": "/static/js/main.eb63d6bb.chunk.js"
+  },
+  {
+    "revision": "d2f225f2fa4a37056413",
+    "url": "/static/js/runtime-main.034d15aa.js"
+  },
+  {
+    "revision": "64b7ec9d075e9f838d795ab19a3d05f0",
+    "url": "/static/media/pokeball.64b7ec9d.png"
+  }
+]);
